2624211152748a921246236116ca9e8d3e96f75d
Jose Luis Olmedo Barionuevo <jolmedo@fp.lasalle.es>
2026-03-12 22:13:07 +0100

    cambios iniciales backend

---
+++ backend/src/test/java/com/appweb/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appweb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
